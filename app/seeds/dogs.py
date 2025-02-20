966f6bf5048e9595d07fc3098bf22cf3ba38bdb4
miaohua897 <haijianhoubj@gmail.com>
2025-02-20 06:41:34 -0800

    updated

--- app/seeds/dogs.py
+++ app/seeds/dogs.py
@@ -9,7 +9,26 @@ def seed_dogs():
         gender='male',
         neutered_spayed='spayed',
         microchip=False,
-        image_url='https://testbucketbymiaohua.s3.us-west-1.amazonaws.com/Untitled+design+(20).png',
+        image_url='https://testbucketbymiaohua.s3.us-west-1.amazonaws.com/pexels-poodles2doodles.jpg',
+        birth_date=date(2023, 5, 10),
+        breed_name ='Siberian Husky',
+        description='info brief',
+        medical_allergies='NA',
+        owner_phone_number='1234556435',
+        owner_email='hello@gmail.com',
+        owner_address_line_one='helloworld ln',
+        owner_address_line_two='helloworld two ln',
+        owner_address_city ='SD',
+        owner_address_state='CA',
+        owner_address_zip_code = 92130,
+        owner_country = "UK",
+        user_id =1)
+    demo_two = Dog(
+        dog_name='pawpaw',age =1,color='white',weight=7.20,
+        gender='female',
+        neutered_spayed='spayed',
+        microchip=False,
+        image_url='https://testbucketbymiaohua.s3.us-west-1.amazonaws.com/pexels-nishizuka-25426-191353.jpg',
         birth_date=date(2023, 5, 10),
         breed_name ='Siberian Husky',
         description='info brief',
@@ -25,6 +44,7 @@ def seed_dogs():
         user_id =1)
        
     db.session.add(demo)
+    db.session.add(demo_two)
     db.session.commit()
 
 def undo_dogs():
