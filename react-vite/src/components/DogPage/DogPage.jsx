9ccd1737c96aca4717db954f99015acbe6568e02
miaohua897 <haijianhoubj@gmail.com>
2025-02-20 06:49:14 -0800

    updated

--- react-vite/src/components/DogPage/DogPage.jsx
+++ react-vite/src/components/DogPage/DogPage.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import {thunkLoadDogs} from '../../redux/dog';
 import './DogPage.css'
 
 function DogPage(){
     const dispatch = useDispatch()
+    const [selectId,setSelectId] = useState(0)
     useEffect(()=>{
             dispatch(thunkLoadDogs())
     },[dispatch])
 
     const dogs = useSelector(state=>state.dog.dog);
     let dogsArr=[];
-    let firstDog;
     if(dogs) {
         dogsArr = Object.values(dogs);
-        firstDog = dogsArr[0];
+  
     }
     // console.log('i am from dog page',dogs,dogsArr,firstDog) 
    
@@ -25,17 +25,17 @@ function DogPage(){
                 dogsArr.length !== 0 ?
                 <div>
                 <p>Happy Doy</p>
-                <h3>{firstDog.dog_name}</h3>
-                <img src={firstDog.image_url} className="dog-info-image" />
-                <p>{firstDog.age}</p>
-                <p>{firstDog.birth_date}</p>
-                <p>{firstDog.color}</p>
-                <p>{firstDog.weight}</p>
-                <p>{firstDog.breed_name}</p>
-                <p>{firstDog.description}</p>
-                <p>{firstDog.gender}</p>
-                <p>{firstDog.medical_allergies}</p>
-                <p>{firstDog.owner.username}</p>
+                <h3>{dogsArr[selectId].dog_name}</h3>
+                <img src={dogsArr[selectId].image_url} className="dog-info-image" />
+                <p>{dogsArr[selectId].age}</p>
+                <p>{dogsArr[selectId].birth_date}</p>
+                <p>{dogsArr[selectId].color}</p>
+                <p>{dogsArr[selectId].weight}</p>
+                <p>{dogsArr[selectId].breed_name}</p>
+                <p>{dogsArr[selectId].description}</p>
+                <p>{dogsArr[selectId].gender}</p>
+                <p>{dogsArr[selectId].medical_allergies}</p>
+                <p>{dogsArr[selectId].owner.username}</p>
                 </div>
                 :<h2>add your first dog</h2>
             }
@@ -44,7 +44,7 @@ function DogPage(){
         {
             dogsArr.length !== 0 ?
             dogsArr.map((dog,index) =>(
-                <div key={index}>
+                <div key={index} onClick={()=> setSelectId(dog.id-1)}>
                     <img src={dog.image_url} className="dog-cards-image"></img>
                     <p>{dog.dog_name}</p>
                 </div>
