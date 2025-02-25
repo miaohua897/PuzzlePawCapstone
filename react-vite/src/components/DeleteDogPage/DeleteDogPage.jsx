c9fc1a0274dff81a9f539f57f80d3982744638de
miaohua897 <haijianhoubj@gmail.com>
2025-02-24 17:09:25 -0800

    updated

--- react-vite/src/components/DeleteDogPage/DeleteDogPage.jsx
+++ react-vite/src/components/DeleteDogPage/DeleteDogPage.jsx
@@ -20,7 +20,7 @@ function DeleteDogPage({dog_id}){
     return (
         <div className="delete-container">
             <form className="delete-form-container">
-                <h1>Delete a Photo</h1>
+                <h1>Delete a Dog</h1>
                 <div className='delete-form-buttons-container'>
                     <button className='delete-form-button' onClick={handleCancelDeleteDog}>Cancel</button>
                     <button className='delete-form-button' onClick={handleDeleteDog}> Delete</button>
