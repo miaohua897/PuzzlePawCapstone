1a13750eab41dc8cba1509a9a0cd88b0145fcc00
miaohua897 <haijianhoubj@gmail.com>
2025-03-08 15:58:09 -0800

    add note-page

--- app/seeds/notes.py
+++ app/seeds/notes.py
@@ -17,8 +17,24 @@ def seed_notes():
        user_id =1,
        dog_id=1 
     )
+    demo2=Note(
+       note_date = date(2024,1,1),
+       title='note3',
+       content='this is third note',
+       user_id =1,
+       dog_id=2 
+    )
+    demo3=Note(
+       note_date = date(2024,1,1),
+       title='note3',
+       content='this is fourth note',
+       user_id =1,
+       dog_id=3 
+    )
     db.session.add(demo)
     db.session.add(demo1)
+    db.session.add(demo2)
+    db.session.add(demo3)
     db.session.commit()
 
 def undo_notes():
--- react-vite/src/components/NotePage/NotePage.jsx
+++ react-vite/src/components/NotePage/NotePage.jsx
@@ -1,6 +1,52 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {thunkLoadNotes} from '../../redux/note';
+import { useSetDogId } from "../../context/SetDogId";
+import SideBarNote from './SideBarNote';
+import { useSideBarStatus } from "../../context/SideBar";
+import {FaArrowLeft} from 'react-icons/fa';
+
 function NotePage(){
+    const dispatch = useDispatch()
+    const notes = useSelector(state=>state.note.note);
+    const dogs = useSelector(state=>state.dog.dog);
+
+    const {selectedDogId} = useSetDogId(); 
+    const {setIsSideBarOpen} = useSideBarStatus();
+    useEffect(()=>{
+        dispatch(thunkLoadNotes())    
+    },[dispatch])
+
+    let notesArr=[];
+    let dogNotesArr=[];
+    if(notes) notesArr = Object.values(notes).reverse();
+    if( notesArr.length !==0) dogNotesArr = notesArr.filter(el=>el.dog_id===selectedDogId)
+    let dogsArr =[];
+    if(dogs) dogsArr = Object.values(dogs);
+
     return (
-        <p>note page</p>
+       <div>
+                     <div className="sidebar-button-container">
+                        <button className='sidebar-button' onClick={()=>{
+                            setIsSideBarOpen(true)
+                            }} >
+                        <FaArrowLeft color='darkblue'/>
+                        </button>
+                    </div>  
+        <SideBarNote dogsArr={dogsArr}/>
+        {dogNotesArr.length !== 0?
+          dogNotesArr.map((note,index)=>{
+            return (
+                <div key={index}>
+                      <h4>{note.title}</h4>
+                      <p>{note.content}</p>
+                </div>
+            )
+          })
+         :<p>your dog have note yet</p> 
+          }
+
+       </div>
     )
 }
 export default NotePage;
